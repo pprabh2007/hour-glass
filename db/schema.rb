38360fba3641c47be1a49f0e247a73f1fab05355
Atharv Vanarase <avv2116@columbia.edu>
2023-10-31 01:58:34 -0400

    avv2116 remove movie db

--- db/schema.rb
+++ db/schema.rb
@@ -35,15 +35,6 @@ ActiveRecord::Schema.define(version: 20231031042319) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "schedules", force: :cascade do |t|
     t.string   "courseId"
     t.datetime "startTime"
