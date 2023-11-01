9ee4409727ece905b635cd5c7466969153e3d158
Atharv Vanarase <avv2116@columbia.edu>
2023-11-01 15:57:55 -0400

    avv2116 update tests

--- app/controllers/teaching_assistants_controller.rb
+++ app/controllers/teaching_assistants_controller.rb
@@ -2,21 +2,6 @@ class TeachingAssistantsController < ApplicationController
     before_action :set_teaching_assistant, only: [:show, :edit, :update, :destroy]
     skip_before_action :authenticate_user, only: [:new_office_hour, :create_office_hour]
   
-    def new
-        @teaching_assistant = TeachingAssistant.new
-      end
-      
-      def create
-        @teaching_assistant = TeachingAssistant.new(teaching_assistant_params)
-      
-        if @teaching_assistant.save
-          flash[:notice] = 'Teaching Assistant was successfully created.'
-        else
-          flash[:alert] = 'Failed to create Teaching Assistant.'
-          render :new
-        end
-      end
-  
     def new_office_hour
         @teaching_assistant = TeachingAssistant.find_by(id: params[:id]) if params[:id] && params[:id] != 'new_office_hour'
         @teaching_assistant ||= TeachingAssistant.new
@@ -34,13 +19,6 @@ class TeachingAssistantsController < ApplicationController
       end
   
     private
-  
-    def teaching_assistant_params
-      params.require(:teaching_assistant).permit(:name, :uni, :class_id)
-    end
-  
-    private
-  
     def calendar_params
       params.require(:calendar).permit(:class_id, :start_time, :end_time, :teaching_assistant_id)
     end
--- spec/controllers/teaching_assistants_controller_spec.rb
+++ spec/controllers/teaching_assistants_controller_spec.rb
@@ -8,33 +8,7 @@ RSpec.describe TeachingAssistantsController, type: :controller do
     end
   end
 
-  describe "private #teaching_assistant_params" do
-  it "permits the correct parameters" do
-    params = ActionController::Parameters.new(teaching_assistant: { name: "John Doe", uni: "jd123", class_id: "COMS101" })
-    permitted_params = params.require(:teaching_assistant).permit(:name, :uni, :class_id).to_h.symbolize_keys
-
-    expect(permitted_params).to eq({ name: "John Doe", uni: "jd123", class_id: "COMS101" })
-  end
-end
-
-describe 'POST #create' do
-  context 'with valid parameters' do
-    it 'creates a new teaching assistant' do
-      post :create, { :name => 'John Doe', :uni => 'jd123', :class_id => 'COMS101' }
-      expect(response).not_to render_template(:new)
-    end
-  end
-
-  context 'with invalid parameters' do
-    it 'does not create a new teaching assistant' do
-      expect {
-        post :create, params: { teaching_assistant: { name: '', uni: '', class_id: '' } }
-      }.not_to change(TeachingAssistant, :count)
-    end
-  end
-end
-
-describe "private #calendar_params" do
+  describe "private #calendar_params" do
     it "permits the correct parameters" do
       params = ActionController::Parameters.new(calendar: { "class_id" => "COMS101", "start_time" => "9:00 AM", "end_time" => "11:00 AM", "teaching_assistant_id" => 1 })
       permitted_params = params.require(:calendar).permit(:class_id, :start_time, :end_time, :teaching_assistant_id)
@@ -42,5 +16,4 @@ describe "private #calendar_params" do
       expect(permitted_params).to eq({ "class_id" => "COMS101", "start_time" => "9:00 AM", "end_time" => "11:00 AM", "teaching_assistant_id" => 1 })
     end
   end
-
 end
