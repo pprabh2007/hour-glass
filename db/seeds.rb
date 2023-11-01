6f8e595a3712e89a12da2d838fc617fdfa006d46
Shivansh Srivastava <40896625+ShivanshSrivastava1@users.noreply.github.com>
2023-11-01 00:02:53 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-courses = [{:courseName => 'COMS 4152', courseDescription: 'Engineering Software-as-a-Service'},
-{:courseName => 'CSOR 4231', courseDescription: 'Intro to Algorithms'},
-{:courseName => 'TestClass', courseDescription: 'Test Description'}]
+courses = [{:courseName => 'COMS 4152', :courseDescription => 'Engineering Software-as-a-Service'},
+{:courseName => 'CSOR 4231', :courseDescription => 'Intro to Algorithms'},
+{:courseName => 'TestClass', :courseDescription => 'Test Description'}]
 
-schedules = [{:courseId => 'a', startTime: DateTime.new(2023, 1, 1, 13, 0), endTime: DateTime.new(2023, 1, 1, 14, 0)},
-{:courseId => 'b', startTime: DateTime.new(2023, 1, 2, 14, 0), endTime: DateTime.new(2023, 1, 2, 15, 0)}]
+schedules = [{:courseId => 'COMS 4152', :startTime=> DateTime.new(2023, 1, 1, 13, 0), :endTime=> DateTime.new(2023, 1, 1, 14, 0)},
+{:courseId => 'CSOR 4231', :startTime=> DateTime.new(2023, 1, 2, 14, 0), :endTime=> DateTime.new(2023, 1, 2, 15, 0)}]
+
+users = [{:uni => "cheems", :password_digest => "password", :created_at=> DateTime.new(2023,1,1),
+:updated_at => DateTime.new(2023,1,1), :name => "Cheems Jr." }];
+
+entitlements = [{:uni => "cheems", :courseId => "COMS 4152", :role => "student", 
+:created_at=> DateTime.new(2023,1,1), :updated_at=>DateTime.new(2023,1,1)}];
 
 courses.each do |course|
 	Course.create!(course)
   end
 
 schedules.each do |schedule|
-    Schedule.create!(schedule)
+  Schedule.create!(schedule)
+end
+
+users.each do |user|
+  User.create(user)
+end
+
+entitlements.each do |entitlement|
+  Entitlement.create(entitlement)
 end
