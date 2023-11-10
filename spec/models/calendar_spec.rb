bf8044588f4b784fe9786fbe72801f20680180c4
Shivansh Srivastava <ss5945@columbia.edu>
2023-11-09 17:54:55 -0700

    Added checkbox for recurring weekly OH time

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Calendar, type: :model do
-  # let(:teaching_assistant) { TeachingAssistant.create(name: 'John Doe') }
-
   it 'is valid with valid attributes' do
-    calendar = Calendar.new(class_id: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour)
+    calendar = Calendar.new(class_id: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour, recurrence: '1')
     expect(calendar).to be_valid
   end
 
   it 'is not valid without a class_id' do
-    calendar = Calendar.new(start_time: DateTime.now, end_time: DateTime.now + 1.hour)
+    calendar = Calendar.new(start_time: DateTime.now, end_time: DateTime.now + 1.hour, recurrence: '1')
     expect(calendar).not_to be_valid
   end
 
   it 'is not valid without a start_time' do
-    calendar = Calendar.new(class_id: 'CS101', end_time: DateTime.now + 1.hour)
+    calendar = Calendar.new(class_id: 'CS101', end_time: DateTime.now + 1.hour, recurrence: '1')
     expect(calendar).not_to be_valid
   end
 
   it 'is not valid without an end_time' do
-    calendar = Calendar.new(class_id: 'CS101', start_time: DateTime.now)
+    calendar = Calendar.new(class_id: 'CS101', start_time: DateTime.now, recurrence: '1')
+    expect(calendar).not_to be_valid
+  end
+
+  it 'is not valid without a recurrence' do
+    calendar = Calendar.new(class_id: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour)
     expect(calendar).not_to be_valid
   end
 end
