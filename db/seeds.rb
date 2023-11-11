40014f9a3b0d31d5b6ca8dc6eea06c46ca41be36
Atharv Vanarase <144260291+avv2116@users.noreply.github.com>
2023-11-11 16:39:31 -0500

    Fix user seed data (#29)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -10,25 +12,17 @@ courses = [{:courseName => 'COMS 4152', :courseDescription => 'Engineering Softw
 {:courseName => 'CSOR 4231', :courseDescription => 'Intro to Algorithms'},
 {:courseName => 'TestClass', :courseDescription => 'Test Description'}]
 
-schedules = [{:courseId => 'COMS 4152', :startTime=> DateTime.new(2023, 1, 1, 13, 0), :endTime=> DateTime.new(2023, 1, 1, 14, 0)},
-{:courseId => 'CSOR 4231', :startTime=> DateTime.new(2023, 1, 2, 14, 0), :endTime=> DateTime.new(2023, 1, 2, 15, 0)}]
-
-users = [{:uni => "cheems", :password_digest => "password", :created_at=> DateTime.new(2023,1,1),
-:updated_at => DateTime.new(2023,1,1), :name => "Cheems Jr." }];
+users = [{:uni => "cheems", :password_digest => BCrypt::Password.create("password"), :name => "Cheems Jr." }];
 
 entitlements = [{:uni => "cheems", :courseId => "COMS 4152", :role => "student", 
 :created_at=> DateTime.new(2023,1,1), :updated_at=>DateTime.new(2023,1,1)}];
 
 courses.each do |course|
 	Course.create!(course)
-  end
-
-calendars.each do |calendar|
-  Calendar.create!(calendar)
 end
 
 users.each do |user|
-  User.create(user)
+  User.create user
 end
 
 entitlements.each do |entitlement|
