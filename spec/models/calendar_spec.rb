8ced5bfd84e175b016d4ddb8ab90ca91d57f9949
Atharv Vanarase <144260291+avv2116@users.noreply.github.com>
2023-11-13 15:42:30 -0500

    Fixes for rspec testing (#37)

--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Calendar, type: :model do
   it 'is valid with valid attributes' do
-    calendar = Calendar.new(courseName: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour, recurrence: '1')
+    calendar = Calendar.new(courseName: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour, repeated_weeks: 5)
     expect(calendar).to be_valid
   end
 
   it 'is not valid without a courseName' do
-    calendar = Calendar.new(start_time: DateTime.now, end_time: DateTime.now + 1.hour, recurrence: '1')
+    calendar = Calendar.new(start_time: DateTime.now, end_time: DateTime.now + 1.hour,  repeated_weeks: 1)
     expect(calendar).not_to be_valid
   end
 
   it 'is not valid without a start_time' do
-    calendar = Calendar.new(courseName: 'CS101', end_time: DateTime.now + 1.hour, recurrence: '1')
+    calendar = Calendar.new(courseName: 'CS101', end_time: DateTime.now + 1.hour, repeated_weeks: 2)
     expect(calendar).not_to be_valid
   end
 
   it 'is not valid without an end_time' do
-    calendar = Calendar.new(courseName: 'CS101', start_time: DateTime.now, recurrence: '1')
+    calendar = Calendar.new(courseName: 'CS101', start_time: DateTime.now, repeated_weeks: 3)
     expect(calendar).not_to be_valid
   end
 
-  it 'is not valid without a recurrence' do
+  it 'is valid without a repeated_weeks as it defaults to 0' do
     calendar = Calendar.new(courseName: 'CS101', start_time: DateTime.now, end_time: DateTime.now + 1.hour)
-    expect(calendar).not_to be_valid
+    expect(calendar).to be_valid
+    expect(calendar.repeated_weeks == 0)
   end
 end
