4c289488c549a052f3793d940d0aa57c1ec58f47
Atharv Vanarase <avv2116@columbia.edu>
2023-11-26 16:02:10 -0500

    Add back 15 minute intervals

--- spec/controllers/teaching_assistants_controller_spec.rb
+++ spec/controllers/teaching_assistants_controller_spec.rb
@@ -56,11 +56,33 @@ RSpec.describe TeachingAssistantsController, type: :controller do
     end
 
 
-    context "with invalid attributes" do
+    context "with nil attributes" do
       it "renders the new_office_hour template" do
-        allow_any_instance_of(Calendar).to receive(:save).and_return(false)
+        session[:user_name] = user.name
+        session[:user_uni] = user.uni
+        session[:user_password] = user.password
+        allow_any_instance_of(Calendar).to receive(:save).and_return(true)
         post :create_office_hour, {:calendar => { "courseName" => nil, "start_time" => nil, "end_time" => nil, "repeated_weeks" => nil } }
         expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_office_hour_teaching_assistants_path) # Should redirect back to the same page after failing
+      end
+    end
+
+    context "with invalid attributes" do
+      it "renders the new_office_hour template" do
+        session[:user_name] = user.name
+        session[:user_uni] = user.uni
+        session[:user_password] = user.password
+        allow_any_instance_of(Calendar).to receive(:save).and_return(true)
+        post :create_office_hour, {:calendar => { "courseName" => "COMS101", "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "12", "end_time(4i)" => "2", "end_time(5i)" => "22", "repeated_weeks" => 0 } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_office_hour_teaching_assistants_path) # Should redirect back to the same page after failing because object not on same day
+      
+        post :create_office_hour, {:calendar => { "courseName" => "COMS101", "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "11", "end_time(4i)" => "2", "end_time(5i)" => "20", "repeated_weeks" => 0 } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_office_hour_teaching_assistants_path) # Should redirect back to the same page after failing because end time before start time
       end
     end
 
@@ -70,8 +92,15 @@ RSpec.describe TeachingAssistantsController, type: :controller do
         session[:user_uni] = user.uni
         session[:user_password] = user.password
         allow_any_instance_of(Calendar).to receive(:save).and_return(true)
-        post :create_office_hour, {:calendar => { "courseName" => "COMS101", "start_time" => "9:00 AM", "end_time" => "11:00 AM", "repeated_weeks" => 0 } }
+        post :create_office_hour, {:calendar => { "courseName" => "COMS101", "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "11", "end_time(4i)" => "2", "end_time(5i)" => "22", "repeated_weeks" => 0 } }
         expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(user_profile_path) # To new user profile page when given a valid non-repeating calendar
+
+        post :create_office_hour, {:calendar => { "courseName" => "COMS101", "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "11", "end_time(4i)" => "2", "end_time(5i)" => "22", "repeated_weeks" => 3 } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(user_profile_path) # To new user profile page when given a valid repeating calendar
       end
     end
   end
@@ -127,18 +156,29 @@ RSpec.describe TeachingAssistantsController, type: :controller do
         session[:user_password] = user.password
         allow(Calendar).to receive(:find).with(calendar.id.to_s).and_return(calendar)
         allow(calendar).to receive(:update).with(any_args).and_return(true)
-        patch :update, {:id => calendar.id, :calendar => { "start_time" => DateTime.now } }
+        patch :update, {:id => calendar.id, :calendar => { "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "11", "end_time(4i)" => "2", "end_time(5i)" => "22" } }
         expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(edit_office_hour_teaching_assistants_path) # Redirect to edit office hour teaching assistant path when successful
       end
     end
 
 
     context "with invalid attributes" do
       it "renders the edit_office_hour template" do
+        session[:user_name] = user.name
+        session[:user_uni] = user.uni
+        session[:user_password] = user.password
         allow(Calendar).to receive(:find).with(calendar.id.to_s).and_return(calendar)
-        allow(calendar).to receive(:update).with(any_args).and_return(false)
+        allow(calendar).to receive(:update).with(any_args).and_return(true)
         patch :update, {:id => calendar.id, :calendar => { "start_time" => nil } }
         expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(edit_calendar_path(calendar)) # Redirect to edit calendar path when failed
+
+        patch :update, {:id => calendar.id, :calendar => { "start_time(1i)" => "2023", "start_time(2i)" =>"11", "start_time(3i)" => "11", "start_time(4i)" => "2", "start_time(5i)" => "21", 
+        "end_time(1i)" => "2023", "end_time(2i)" =>"11", "end_time(3i)" => "11", "end_time(4i)" => "2", "end_time(5i)" => "20" } }
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(edit_calendar_path(calendar)) # Redirect to edit calendar path when failed
       end
     end
 
