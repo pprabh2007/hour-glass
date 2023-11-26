ec72f9f1732fa1339ecffc20628238951dfaef77
Atharv Vanarase <144260291+avv2116@users.noreply.github.com>
2023-11-26 16:06:08 -0500

    Add back 15 minute intervals (#56)

--- app/controllers/teaching_assistants_controller.rb
+++ app/controllers/teaching_assistants_controller.rb
@@ -8,27 +8,32 @@ class TeachingAssistantsController < ApplicationController
   end
 
   def create_office_hour
-    @calendar = current_user.calendars.build(calendar_params)
-    if @calendar.save
-      if @calendar.repeated_weeks.positive?
-        start_time = @calendar.start_time.advance(weeks: 1)
-        end_time = @calendar.end_time.advance(weeks: 1)
-        repeated_weeks = @calendar.repeated_weeks
-  
-        while start_time <= repeated_weeks.weeks.from_now(@calendar.start_time)
-          new_calendar = current_user.calendars.build(
-            courseName: @calendar.courseName,
-            start_time: start_time,
-            end_time: end_time,
-            repeated_weeks: @calendar.repeated_weeks
-          )
-          new_calendar.save
-          start_time = start_time.advance(weeks: 1)
-          end_time = end_time.advance(weeks: 1)
+    if not valid_calendar
+      flash[:warning] = 'All calendar events must end on the same day and strictly after the start time'
+      redirect_to new_office_hour_teaching_assistants_path
+    elsif
+      @calendar = current_user.calendars.build(calendar_params)
+      if @calendar.save
+        if @calendar.repeated_weeks.positive?
+          start_time = @calendar.start_time.advance(weeks: 1)
+          end_time = @calendar.end_time.advance(weeks: 1)
+          repeated_weeks = @calendar.repeated_weeks
+    
+          while start_time <= repeated_weeks.weeks.from_now(@calendar.start_time)
+            new_calendar = current_user.calendars.build(
+              courseName: @calendar.courseName,
+              start_time: start_time,
+              end_time: end_time,
+              repeated_weeks: @calendar.repeated_weeks
+            )
+            new_calendar.save
+            start_time = start_time.advance(weeks: 1)
+            end_time = end_time.advance(weeks: 1)
+          end
+          redirect_to user_profile_path, notice: 'Office hours were successfully added.'
+        else
+          redirect_to user_profile_path, notice: 'Office hour was successfully added.'
         end
-        redirect_to user_profile_path, notice: 'Office hours were successfully added.'
-      else
-        redirect_to user_profile_path, notice: 'Office hour was successfully added.'
       end
     end
   end
@@ -44,7 +49,10 @@ class TeachingAssistantsController < ApplicationController
 
   def update
     @calendar = Calendar.find(params[:id])
-    if @calendar.update(calendar_params)
+    if not valid_calendar
+      flash[:warning] = 'All calendar events must end on the same day and strictly after the start time'
+      redirect_to edit_calendar_path(@calendar)
+    elsif @calendar.update(calendar_params)
       redirect_to edit_office_hour_teaching_assistants_path, notice: 'Office hour was successfully updated.'
     end
   end
@@ -52,5 +60,25 @@ class TeachingAssistantsController < ApplicationController
   private
   def calendar_params
     params.require(:calendar).permit(:courseName, :start_time, :end_time, :repeated_weeks)
+  end
+
+  private
+  def valid_calendar
+    params = calendar_params
+    any_field_missing = params["start_time(1i)"].nil? or params["start_time(2i)"].nil? or params["start_time(3i)"].nil? or params["start_time(4i)"].nil? or params["start_time(5i)"].nil? or params["end_time(1i)"].nil? or params["end_time(2i)"].nil? or params["end_time(3i)"].nil? or params["end_time(4i)"].nil? or params["end_time(5i)"].nil?
+    if any_field_missing
+      return false
+    end
+    start_year = params["start_time(1i)"].to_i
+    start_month = params["start_time(2i)"].to_i
+    start_day = params["start_time(3i)"].to_i
+    start_hour = params["start_time(4i)"].to_i
+    start_minute = params["start_time(5i)"].to_i
+    end_year = params["end_time(1i)"].to_i
+    end_month = params["end_time(2i)"].to_i
+    end_day = params["end_time(3i)"].to_i
+    end_hour = params["end_time(4i)"].to_i
+    end_minute = params["end_time(5i)"].to_i
+    Date.new(start_year, start_month, start_day) === Date.new(end_year, end_month, end_day) and DateTime.new(start_year, start_month, start_day, start_hour, start_minute) < DateTime.new(end_year, end_month, end_day, end_hour, end_minute)
   end
 end
