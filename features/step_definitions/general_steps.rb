6fea87e1560ef3def218100e462da5c65b17b061
Atharv Vanarase <144260291+avv2116@users.noreply.github.com>
2023-12-16 21:46:56 -0500

    Create remove viewer button (#63)

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -5,11 +5,15 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
 end
 
 Then /I should not see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
   index_1 = page.body.index(e1)
   index_2 = page.body.index(e2)
   expect(index_1.nil? || (index_2.nil? == false && index_1 > index_2))
+end
+
+Then /I should not see "(.*)" after "(.*)"/ do |e1, e2|
+  index_1 = page.body.index(e1)
+  index_2 = page.body.index(e2)
+  expect(index_1.nil? || (index_2.nil? == false && index_1 < index_2))
 end
 
 # When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
--- spec/controllers/entitlements_controller_spec.rb
+++ spec/controllers/entitlements_controller_spec.rb
@@ -4,8 +4,10 @@ require 'set'
 RSpec.describe EntitlementsController, type: :controller do
     let(:existingUser) { User.create(name: "testStudent", uni: "testStudentUni", password: "testPassword") }
     let(:existingViewerEntitlement) { Entitlement.create(uni: "testStudentUni",  courseName: "COMS 4152", role: "Viewer" ) }
+    let(:existingTAEntitlement) { Entitlement.create(uni: "testStudentUni",  courseName: "class3", role: "TA" ) }
     let(:firstCourse) { Course.create( courseName: "COMS 4152", courseDescription: "Engineering Software-as-a-Service", created_at: DateTime.new(2023, 1, 1), updated_at: DateTime.new(2023, 1, 1)) } 
     let(:secondCourse) { Course.create( courseName: "CSOR 4231", courseDescription: "Algos", created_at: DateTime.new(2023, 1, 1), updated_at: DateTime.new(2023, 1, 1)) } 
+    let(:thirdCourse) { Course.create( courseName: "class3", courseDescription: "class3", created_at: DateTime.new(2023, 1, 1), updated_at: DateTime.new(2023, 1, 1)) } 
 
     before do
         allow(controller).to receive(:current_user).and_return(existingUser)
@@ -40,6 +42,45 @@ RSpec.describe EntitlementsController, type: :controller do
             end
         end
     end
+
+    describe "POST #delete_viewer" do
+    context "with a valid courseName" do
+        it "appropriately removes the class if the user currently has viewer access" do
+            session[:user_uni] = existingUser.uni
+            post :delete_viewer, entitlement: {courseName: existingViewerEntitlement.courseName}  # Current user has viewer entitlement already
+            expect(flash[:notice]).to eq("Removed the class '#{existingViewerEntitlement.courseName}' from your schedule")
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(user_profile_path)
+            expect(Entitlement.find_by(uni: existingUser.uni, courseName: existingViewerEntitlement.courseName)).to eq(nil)
+        end
+
+        it "does not remove the class if the user is an admin" do
+            session[:user_uni] = existingUser.uni
+            post :delete_viewer, entitlement: {courseName: existingTAEntitlement.courseName} # Current user has TA entitlement already
+            expect(flash[:warning]).to eq("Cannot remove the class as you are an administrator for the class")
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(user_profile_path) # To user profile page
+        end
+
+        it "does not remove the class if the user has no entitlements" do
+            session[:user_uni] = existingUser.uni
+            post :delete_viewer, entitlement: {courseName: thirdCourse.courseName} # Current user has no entitlements
+            expect(flash[:warning]).to eq("Could not find entitlements for '#{thirdCourse.courseName}' in your schedule")
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(user_profile_path) # To user profile page
+        end
+    end
+
+    context "with an empty course name" do
+        it "does not remove the class if the user has no entitlements" do
+            session[:user_uni] = existingUser.uni
+            post :delete_viewer, entitlement: {courseName: ''} # Current user has no entitlements since it doesnt exist
+            expect(flash[:warning]).to eq("Could not find entitlements for \'\' in your schedule")
+            expect(response).to have_http_status(:redirect)
+            expect(response).to redirect_to(user_profile_path) # To user profile page
+        end
+    end
+end
 
     describe "private #entitlement_params" do
         it "permits the correct parameters" do
